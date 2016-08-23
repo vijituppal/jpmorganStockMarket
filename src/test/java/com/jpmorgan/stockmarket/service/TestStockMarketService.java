2634d62ab2e70431fa25e86c4d3abe3d63eb138a
Varad <Varad@192.168.1.217>
2016-08-23 03:34:24 +0100

    Junit Test added

--- src/test/java/com/jpmorgan/stockmarket/service/TestStockMarketService.java
+++ src/test/java/com/jpmorgan/stockmarket/service/TestStockMarketService.java
@@ -1,5 +1,54 @@
 package com.jpmorgan.stockmarket.service;
 
-public class TestStockMarketService {
+import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jpmorgan.stockmarket.domain.Stock;;
+
+public class TestStockMarketService {
+	
+	StockMarketService service;
+	List<Stock> stockList;
+	Stock stock;
+	
+	@Before
+	public void setUp(){
+		 service= new StockMarketService();
+		 stockList = service.getStocks();
+		 stock = stockList.get(3);
+		 
+	}
+	@Test
+	public void testDividendYieldCommonStock(){
+		stock = stockList.get(2);
+		assertNotNull(service.getDividendYield(stock, new BigDecimal(100)));
+		assertEquals(new BigDecimal("0.23"), service.getDividendYield(stock, new BigDecimal(100)));
+	}
+	
+	@Test
+	public void testDividendYieldPreferredStock(){
+		assertNotNull(service.getDividendYield(stock, new BigDecimal(100)));
+		assertEquals(new BigDecimal("0.02"), service.getDividendYield(stock, new BigDecimal(100)));
+		
+	}
+	
+	@Test
+	public void testPERatio(){
+		assertNotNull(service.getPERatio(stock, new BigDecimal(100)));
+		assertEquals(new BigDecimal(12.5), service.getPERatio(stock, new BigDecimal(100)));
+	}
+	
+	@After
+	public void cleanUp(){
+		 service= null;
+		 stockList = null;
+		 stock = null;
+		 
+	}
 }
--- src/test/java/com/jpmorgan/stockmarket/service/TestTradeDetailsService.java
+++ src/test/java/com/jpmorgan/stockmarket/service/TestTradeDetailsService.java
@@ -1,5 +1,53 @@
 package com.jpmorgan.stockmarket.service;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Random;
+
+import static com.jpmorgan.stockmarket.domain.StockType.PREFERRED;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.jpmorgan.stockmarket.domain.Stock;
+import com.jpmorgan.stockmarket.domain.Trade;
+import com.jpmorgan.stockmarket.domain.TradeType;
+
 public class TestTradeDetailsService {
+	
+	TradeDetailsService tradeDetails=new TradeDetailsService();
+	StockMarketService service;
+	List<Stock> stockList;
+	Random random = new Random();
+	
+	@Before
+	public void setUp(){
+		service= new StockMarketService();
+		stockList = service.getStocks();
+    	
+	}
+    
+   
+		 
+    @Test
+    public void testVolumeWeightedStockPrice(){
+    	Stock stock = new Stock("GIN", PREFERRED,new BigDecimal("8"), new BigDecimal("0.02"),new BigDecimal("100"));
+    	tradeDetails.addTrade(new Trade(stock, 10, TradeType.BUY,10d));
+    	tradeDetails.addTrade(new Trade(stock, 15, TradeType.BUY,12d));
+    	double expectedResult = (10d*10 + 12d*15) / (10+15);
+    	assertNotNull(tradeDetails.getVolumeWeightedStockPrice(stock));
+    	assertEquals(expectedResult, tradeDetails.getVolumeWeightedStockPrice(stock),0);
+    }
+    
+    @Test
+    public void testGBCEAllShareIndex(){
+    	
+    	tradeDetails.addTrade(new Trade(stockList.get(1), 10, TradeType.BUY,100d));
+    	tradeDetails.addTrade(new Trade(stockList.get(2), 15, TradeType.BUY,120d));
+    	tradeDetails.addTrade(new Trade(stockList.get(3), 12, TradeType.SELL,110d));
+    	tradeDetails.addTrade(new Trade(stockList.get(2), 15, TradeType.BUY,250d));
+    	tradeDetails.addTrade(new Trade(stockList.get(3), 20, TradeType.SELL,300d));
+    	assertEquals(Math.pow(100d*120d*110d*250d*300d, 1.0/5.0), tradeDetails.getGBCEAllShareIndex().doubleValue(), 0);
+    }
 
 }
